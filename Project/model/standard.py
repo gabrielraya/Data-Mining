70ae124448fd1118655b2d79d412e70da0cee358
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-12-03 17:06:14 +0100

    use less parameters in standard network

--- Project/model/standard.py
+++ Project/model/standard.py
@@ -3,21 +3,21 @@ import torch
 
 def StandardModel(load_from=None):
     result = torch.nn.Sequential(
-        torch.nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1),
+        torch.nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, padding=1),
         torch.nn.MaxPool2d(2),
         torch.nn.ReLU(),
-        torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+        torch.nn.Conv2d(in_channels=12, out_channels=16, kernel_size=3, padding=1),
         torch.nn.MaxPool2d(2),
         torch.nn.ReLU(),
-        torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+        torch.nn.Conv2d(in_channels=16, out_channels=24, kernel_size=3, padding=1),
         torch.nn.MaxPool2d(2),
         torch.nn.ReLU(),
         torch.nn.Flatten(),
-        torch.nn.Linear(1024, 512),
+        torch.nn.Linear(384, 256),
         torch.nn.ReLU(),
-        torch.nn.Linear(512, 256),
+        torch.nn.Linear(256, 128),
         torch.nn.ReLU(),
-        torch.nn.Linear(256, 2)
+        torch.nn.Linear(128, 2)
     )
     if load_from is not None:
         result.load_state_dict(torch.load(load_from))
