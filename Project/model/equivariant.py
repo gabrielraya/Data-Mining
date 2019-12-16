42c3f4086334bf488118bd4833a416304b6599ed
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-12-16 18:20:28 +0100

    use pooling between convolutional and dense layers of equivariant model

--- Project/model/equivariant.py
+++ Project/model/equivariant.py
@@ -1,19 +1,32 @@
 import torch
-from model.standard import StandardModel
+from model.base import BaseModel
 
 
-class EquivariantModel(torch.nn.Module):
-    def __init__(self, load_from=None):
-        super(EquivariantModel, self).__init__()
-        self.standard = StandardModel()
-        if load_from is not None:
-            self.load_state_dict(torch.load(load_from))
-
+class EquivariantModel(BaseModel):
     def forward(self, inputs):
-        rotoflips = all_flips(inputs) + all_flips(inputs.permute(0, 1, 3, 2))
-        results = [self.standard(rotoflip) for rotoflip in rotoflips]
-        return torch.stack(results, dim=1).mean(dim=1)
+        rotoflips = all_rotoflips(inputs)
+        convolved = [self.convolutional(rotoflip) for rotoflip in rotoflips]
+        rotoflipped_back = undo_rotoflips(convolved)
+        mean_pooled = torch.mean(torch.stack(rotoflipped_back, dim=0), dim=0)
+        return self.dense(mean_pooled)
 
 
-def all_flips(images):
-    return [images, torch.flip(images, [-1]), torch.flip(images, [-2]), torch.flip(images, [-1, -2])]
+def all_rotoflips(images):
+    return apply_rotoflips([images] * 8)
+
+
+def apply_rotoflips(images):
+    '''Given a list of image batches, rotate/flip each batch differently'''
+    assert len(images) == 8
+    return apply_flips(images[:4]) + apply_flips([image.permute(0, 1, 3, 2) for image in images[4:]])
+
+
+def undo_rotoflips(images):
+    assert len(images) == 8
+    return apply_flips(images[:4]) + [image.permute(0, 1, 3, 2) for image in apply_flips(images[4:])]
+
+
+def apply_flips(images):
+    assert len(images) == 4
+    axes = [[], [-1], [-2], [-1, -2]]
+    return [torch.flip(image, axis) for image, axis in zip(images, axes)]
--- Project/model/standard.py
+++ Project/model/standard.py
@@ -1,24 +1,7 @@
-import torch
+from model.base import BaseModel
 
 
-def StandardModel(load_from=None):
-    result = torch.nn.Sequential(
-        torch.nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, padding=1),
-        torch.nn.MaxPool2d(2),
-        torch.nn.ReLU(),
-        torch.nn.Conv2d(in_channels=12, out_channels=16, kernel_size=3, padding=1),
-        torch.nn.MaxPool2d(2),
-        torch.nn.ReLU(),
-        torch.nn.Conv2d(in_channels=16, out_channels=24, kernel_size=3, padding=1),
-        torch.nn.MaxPool2d(2),
-        torch.nn.ReLU(),
-        torch.nn.Flatten(),
-        torch.nn.Linear(384, 256),
-        torch.nn.ReLU(),
-        torch.nn.Linear(256, 128),
-        torch.nn.ReLU(),
-        torch.nn.Linear(128, 2)
-    )
-    if load_from is not None:
-        result.load_state_dict(torch.load(load_from))
-    return result
+class StandardModel(BaseModel):
+    def forward(self, inputs):
+        convolved = self.convolutional(inputs)
+        return self.dense(convolved)
