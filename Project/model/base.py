c9c798413825ee5177198738849ec4e332452135
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-12-19 13:01:23 +0100

    Revert "reduce base model parameter count"
    This reverts commit e358da08f385b15e6ea9d15154120caacf958675.

--- Project/model/base.py
+++ Project/model/base.py
@@ -5,23 +5,23 @@ class BaseModel(torch.nn.Module):
     def __init__(self, load_from=None):
         super(BaseModel, self).__init__()
         self.convolutional = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, padding=1),
-            torch.nn.MaxPool2d(2),
-            torch.nn.ReLU(),
-            torch.nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=1),
+            torch.nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1),
             torch.nn.MaxPool2d(2),
             torch.nn.ReLU(),
             torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            torch.nn.MaxPool2d(2),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
             torch.nn.MaxPool2d(2),
             torch.nn.ReLU()
         )
         self.dense = torch.nn.Sequential(
             torch.nn.Flatten(),
+            torch.nn.Linear(1024, 512),
+            torch.nn.ReLU(),
             torch.nn.Linear(512, 256),
             torch.nn.ReLU(),
-            torch.nn.Linear(256, 128),
-            torch.nn.ReLU(),
-            torch.nn.Linear(128, 2)
+            torch.nn.Linear(256, 2)
         )
         if load_from is not None:
             self.load_state_dict(torch.load(load_from))
